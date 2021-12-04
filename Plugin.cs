de4bf80e96fa7494e00bc7d1ba010a6a1852b0b6
Captain Ravioli <48518572+Captain-Ravioli@users.noreply.github.com>
2021-12-04 13:25:06 +0200

    Pushed mod

---
+++ Plugin.cs
@@ -0,0 +1,57 @@
+﻿using BepInEx;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UKOneHit
+{
+    [BepInPlugin("cap.ultrakill.onehit", "UKOneHit", "1.0.0")]
+    [BepInProcess("ULTRAKILL.exe")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private NewMovement player = null;
+
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+
+        private void Start()
+        {
+            SceneManager.activeSceneChanged += OnSceneChanged;
+        }
+
+        private void OnSceneChanged(Scene from, Scene to)
+        {
+            Debug.Log($"Scene changed to {to.name}");
+            player = null;
+            if (SceneManager.GetActiveScene().name.StartsWith("Level"))
+            {
+                player = FindObjectOfType<NewMovement>();
+                Debug.Log($"Found player! name:{player.gameObject.name}");
+                Invoke("ModifyHP", 0.01f);
+            }
+        }
+
+        private void Update()
+        {
+            if (player != null)
+            {
+                if (player.dead && Input.GetKeyDown(KeyCode.R))
+                {
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+                }
+            }
+        }
+
+        private void ModifyHP()
+        {
+            if (player != null)
+            {
+                player.antiHp = 99;
+                player.hp = 1;
+                Invoke("ModifyHP", 0.01f);
+            }
+        }
+    }
+}
---
+++ obj/Debug/netstandard2.0/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace UKOneHit
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "UKOneHit";
+        public const string PLUGIN_NAME = "UKOneHit";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
